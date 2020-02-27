8bcb3f4744d3e0a3cf1fc504322c1843e68ca21d
Romilly Cocking <romilly.cocking@gmail.com>
2020-02-27 15:10:59 +0000

    Arduino prototype working.

--- src/arduino/master_reader/master_reader.ino
+++ src/arduino/master_reader/master_reader.ino
@@ -1,11 +1,7 @@
-// Wire Master Reader
+// Reader used to test the prototype I2C Keypad device
+
+// Adapted from Wire Master Reader
 // by Nicholas Zambetti <http://www.zambetti.com>
-
-// Demonstrates use of the Wire library
-// Reads data from an I2C/TWI slave device
-// Refer to the "Wire Slave Sender" example for use with this
-
-// Created 29 March 2006
 
 // This example code is in the public domain.
 
@@ -13,19 +9,19 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin();        // join i2c bus (address optional for master)
+  Wire.begin();        // join i2c bus
   Serial.begin(9600);  // start serial for output
 }
 
 void loop() {
   Wire.requestFrom(8, 1);    // request 6 bytes from slave device #8
 
-  while (Wire.available()) { // slave may send less than requested
+  while (Wire.available()) {
     char c = Wire.read(); // receive a byte as character
     if (c) {
     Serial.print(c);         // print the character
     }
   }
 
-  delay(500);
+  delay(100); // check every 0.1 seconds
 }
